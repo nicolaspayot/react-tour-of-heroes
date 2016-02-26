9f151f9784716eec05de4981c432b6b4d4dd5496
Nicolas Payot <nicolas.payot@gmail.com>
2016-02-26 14:31:14 +0100

    feat(heroes): add selected hero from list

--- src/app/hero/list/heroes.js
+++ src/app/hero/list/heroes.js
@@ -2,21 +2,46 @@ import React, { Component } from 'react';
 import './heroes.scss';
 
 export class Heroes extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+
+  onSelect(selectedHero) {
+    this.setState({ selectedHero });
+  }
+
   render() {
     const heroes = this.props.data;
     const heroNodes = heroes.map(hero => {
       return (
-        <li key={hero.id}>
+        <li
+          key={hero.id}
+          className={this.state.selectedHero === hero ? 'selected' : null }
+          onClick={() => this.onSelect(hero)}>
           <span className="badge">{hero.id}</span> {hero.name}
         </li>
       );
     });
+
+    let selectedHeroNode;
+    if (this.state.selectedHero) {
+      selectedHeroNode = (
+        <div>
+          <h2>{this.state.selectedHero.name.toUpperCase()} is my hero</h2>
+          <button>View Details</button>
+        </div>
+      );
+    }
+
     return (
       <div>
         <h2>My heroes</h2>
         <ul className="heroes">
           {heroNodes}
         </ul>
+        {selectedHeroNode}
       </div>
     );
   }
