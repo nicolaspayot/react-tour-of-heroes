35499e98f16c951fe511bb668db1920d08ed9cef
Nicolas Payot <nicolas.payot@gmail.com>
2016-03-20 21:08:34 +0100

    feat(router): redirect index to dashboard

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { Router, Route, hashHistory, IndexRoute } from 'react-router';
+import { Router, Route, hashHistory, IndexRedirect } from 'react-router';
 import { App } from './app/app';
 import { Dashboard } from './app/dashboard/dashboard';
 import { Heroes } from './app/hero/list/heroes';
@@ -11,7 +11,7 @@ import './index.scss';
 render((
   <Router history={hashHistory}>
     <Route path="/" component={App}>
-      <IndexRoute component={Dashboard}/>
+      <IndexRedirect to="/dashboard"/>
       <Route path="/dashboard" component={Dashboard}/>
       <Route path="/heroes" component={Heroes}/>
       <Route path="/heroes/:id" component={HeroDetail}/>
